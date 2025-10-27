91ed9806fd0e321e8631425b0207e6c161f73714
Xiang <xiangxzou@global.tencent.com>
2025-10-27 11:20:56 +0100

    add requirements.txt and a mini test for data loader

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 ### execute frontal face filter
+
+from facefilter.loader import ImageLoader
+
+loader = ImageLoader("/Users/xiangxzou/Desktop/nn_FACE")
+
+for path, img, meta in loader.iter_images():
+    print(meta.width, meta.height, meta.ext)
